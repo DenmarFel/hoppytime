8b2ccb1d089bc87d001c69aab9ee1dafd327efb0
DenmarFel <denmarfeliciano@gmail.com>
2021-08-26 22:32:55 -0700

    Got links and timestamp selection working

--- src/App.js
+++ src/App.js
@@ -21,19 +21,28 @@ class Player extends React.Component {
 
       const firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
     } else {
       this.loadVideo();
     }
   };
 
+  componentDidUpdate(prevProps) {
+    // Load New Video if new videoId
+    if (prevProps.videoId !== this.props.videoId) {
+      this.state.videoPlayer.loadVideoById(this.props.videoId);
+      this.setState({
+        videoStatus: 'pause'
+      })
+    }
+  }
+
   loadVideo() {
     let videoPlayer = new window.YT.Player('video', {
-      videoId: 'dZ47VLoL514',
+      videoId: this.props.videoId
     });
     this.setState({
       videoPlayer: videoPlayer
-    });
+    })
   }
 
   toggleVideo() {
@@ -55,6 +64,7 @@ class Player extends React.Component {
   render() {
     return (
       <div id="player">
+        {this.props.videoId}
         <div id="video"></div>
         <div id="controls">
           <MediaButton 
@@ -78,10 +88,64 @@ function MediaButton(props) {
   )
 }
 
-export default class App extends React.Component {
+class PlayerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+
+  handleSubmit(event) {
+    console.log(1);
+    this.props.onVideoChange(this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <Player />
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Video Link:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleVideoChange = this.handleVideoChange.bind(this);
+    this.state = {
+      videoId: 'dZ47VLoL514'
+    }
+  }
+
+  handleVideoChange(videoId) {
+    this.setState({
+      videoId: videoId,
+    })
+  }
+
+  render() {
+    const videoId = this.state.videoId;
+    console.log(3);
+    console.log(videoId);
+    return (
+      <div id="app">
+        <PlayerForm 
+          onVideoChange={this.handleVideoChange}/>
+        <Player 
+          videoId={videoId}/>
+      </div>
     );
   }
 }
